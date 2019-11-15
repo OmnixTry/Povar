0cd8d83455b8606279186de97da65d7302bea1cb
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-11-15 19:00:13 +0200

    mistakes fixed + level getter added to student class

--- source/source/user.cpp
+++ source/source/user.cpp
@@ -4,69 +4,90 @@
 #include "user.h"
 using namespace std;
 
-user::user(string logn, string passwd) {
-	login = logn;
-	password = passwd;
-}
-
-bool add_user(string logn, string passwd, string accounttype) {
+bool add_user(string logn, string passwd, string accountType) 
+{
 	user *new_user;
-	if (accounttype == "teacher")
+	if (accountType == "teacher")
 	{
 		new_user = new teacher(logn, passwd);
-		new_user->write_to_file();
+		new_user->write_to_file(accountType);
 		return true;
 	}
-	else if (accounttype == "student")
+	else if (accountType == "student")
 	{
 		new_user = new student(logn, passwd);
-		((teacher *)new_user)->write_to_file();
+		new_user->write_to_file(accountType);
 		return true;
 	}
 	return false;
 }
 
-void user::write_to_file() {
+void user::write_to_file(string accountType) 
+{
 	const string users_file_name = "..\\data\\users.txt";
 	ofstream users(users_file_name, ios::app);
-	string buff = "teacher " + login + " " + password + "\n";
+	string buff; 
+	if (accountType == "student")
+	{
+		buff = accountType + " " + login + " " + password + " 0\n";
+	}
+	else
+	{
+		buff = accountType + " " + login + " " + password + "\n";
+	}
 	users << buff;
 	users.close();
 }
 
-void student::write_to_file() {
-	const string users_file_name = "..\\data\\users.txt";
-	ofstream users(users_file_name, ios::app);
-	string buff = "student " + login + " " + password + " 0" + "\n";
-	users << buff;
-	users.close();
+void parse_user_data(string dataLine, string logn, string &passwd, string &accountType, float &lvl) {
+	int tmp = dataLine.find_first_of(' ');
+	accountType = dataLine.substr(0, tmp);
+	tmp = accountType.size() + 1 + logn.size() + 1;
+	passwd = dataLine.substr(tmp, dataLine.find(' ', tmp + 1) - tmp - 1);
+	tmp += 1 + passwd.size();
+	if (tmp <= dataLine.size())
+	{
+		lvl = stof(dataLine.substr(tmp, dataLine.size() - tmp));
+	}
+	else
+	{
+		lvl = -1;
+	}
 }
 
-bool user_data(string logn, string &passwd, string &accounttype, float &lvl) {
+bool user_data(string logn, string &passwd, string &accountType, float &lvl) 
+{
 	const string dir_file = "..\\data\\users.txt";
 	ifstream user_data_file(dir_file);
-
+	
 	string clogin;
 	string buff;
 	int tmp = 0;
-	while (!user_data_file.eof()) {
+	while (!user_data_file.eof()) 
+	{
 		getline(user_data_file, buff);
 		clogin = buff.substr(buff.find_first_of(' ') + 1, buff.find(' ', buff.find_first_of(' ') + 1) - buff.find_first_of(' ') - 1);
 	
-		if (logn == clogin) {
-			tmp = buff.find_first_of(' ');
-			accounttype = buff.substr(0, tmp);
-			tmp = accounttype.size() + 1 + logn.size() + 1;
-			passwd = buff.substr(tmp, buff.find(' ', tmp+1) - tmp - 1);
-			tmp += 1 + passwd.size();
-			if (tmp <= buff.size()) {
-				lvl = stof(buff.substr(tmp, buff.size() - tmp));
-			}
-			else {
-				lvl = -1;
-			}
+		if (logn == clogin) 
+		{	
+			parse_user_data(buff, logn, passwd, accountType, lvl);
 			return true;
 		}
 	}
-	return false;
+	return false;	
+}
+
+string user::get_login() 
+{
+	return login;
+}
+
+string user::get_password()
+{
+	return password;
+}
+
+float student::get_level() 
+{
+	return level;
 }
--- source/source/user.h
+++ source/source/user.h
@@ -2,36 +2,40 @@
 #include <string>
 using namespace std;
 
-bool add_user(string logn, string passwd, string accounttype);
+bool add_user(string logn, string passwd, string accountType);
 // returns false when something went wrong!!!
 // instruction to use function:
 // pass this function arguments which are login, password and type of account
 // types of accounts are teacher and student
 // if function returns false, account type passed to the function is wrong
 
-bool user_data(string logn, string &passwd, string &accounttype, float &lvl);
-
+bool user_data(string logn, string &passwd, string &accountType, float &lvl);
+// pass this function login of the user you want to find data for
+// also pass the variables you want to save user information in
+// if lvl variable becomes equal to -1 it means that the ackount is teacher(he has no level)
+// if function returns false that means there`s no such user
 
 class user
 {
-public:
-	user(string logn, string passwd);
-	~user() {};
-	friend bool add_user(string logn, string passwd, string accounttype);
 protected:
-	virtual void write_to_file();
+	void write_to_file(string accountType);
 	string login;
 	string password;
+public:
+	string get_login();
+	string get_password();
+	user(string logn, string passwd) : login(logn), password(passwd) {};
+	~user() {};
+	friend bool add_user(string logn, string passwd, string accountType);
 };
 
 class student : public user
 {
 public:
+	float get_level();
 	student(string logn, string passwd, float lvl = 0) : user(logn, passwd), level(lvl) {};
 	~student() {};
-
 private:
-	virtual void write_to_file();
 	float level;
 };
 
@@ -40,7 +44,5 @@ class teacher : public user
 public:
 	teacher(string logn, string passwd) : user(logn, passwd) {};
 	~teacher() {};
-	friend bool add_user(string logn, string passwd, string accounttype);
-private:
-	
+	friend bool add_user(string logn, string passwd, string accountType);
 };
